194cdec50fcbf14caf0917198f3ce36e1f6b916d
Arsenush <arsen.nykytenko@gmail.com>
2017-04-16 21:02:20 +0300

    Initial commit

---
+++ src/main/java/com/hospital/HeartifyApplication.java
@@ -0,0 +1,12 @@
+package com.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartifyApplication.class, args);
+	}
+}
